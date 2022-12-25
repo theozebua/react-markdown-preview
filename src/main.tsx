e2fb1e610600478f29d5b207754e6f20bb4bcb4b
theozebua <ronizebua.rz@gmail.com>
2022-12-25 19:07:14 +0700

    Add `AppLayout.tsx` to `main.tsx`

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AppLayout from './components/layouts/AppLayout'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AppLayout>
+      <App />
+    </AppLayout>
+  </React.StrictMode>
 )
