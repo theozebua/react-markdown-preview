086f8f54b4e2ef786f774e2e6dbd30f282aca7aa
theozebua <ronizebua.rz@gmail.com>
2022-12-25 20:40:50 +0700

    Add `Navbar.tsx` and `Footer.tsx` to `AppLayout.tsx`

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,9 +1,17 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
+import Footer from "../partials/Footer";
+import Navbar from "../partials/Navbar";
 
 interface Props {
-  children: ReactNode
+	children: ReactNode;
 }
 
 export default function AppLayout({ children }: Props): JSX.Element {
-  return <>{children}</>
+	return (
+		<div className="min-h-screen flex flex-col">
+			<Navbar />
+			{children}
+			<Footer />
+		</div>
+	);
 }
