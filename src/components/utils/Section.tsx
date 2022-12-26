80de83afa75ea6d6f91b6224ca11eb554a3196e3
theozebua <ronizebua.rz@gmail.com>
2022-12-26 12:56:27 +0700

    Create `Section.tsx`

---
+++ src/components/utils/Section.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+interface Props {
+	children: ReactNode;
+	className?: String;
+}
+
+export default function Section({ children, className }: Props): JSX.Element {
+	return (
+		<section className={`bg-white w-full shadow p-4 rounded ${className}`}>
+			{children}
+		</section>
+	);
+}
