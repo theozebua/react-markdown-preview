bc5bbfd77ad34f3861ddf88924231e88a0eb85c7
theozebua <ronizebua.rz@gmail.com>
2022-12-27 20:53:30 +0700

    Create markdown functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,50 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { WrenchScrewdriverIcon } from "@heroicons/react/24/outline";
-import { useRef, useEffect, RefObject } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Remarkable } from "remarkable";
+import hljs from "highlight.js";
 import Container from "./components/utils/Container";
 import Section from "./components/utils/Section";
 
 export default function App(): JSX.Element {
-	const textarea: RefObject<HTMLTextAreaElement> =
-		useRef<HTMLTextAreaElement>(null);
+	const [markdownInput, setMarkdownInput] = useState("");
+	const div = useRef<HTMLDivElement>(null);
+	const textarea = useRef<HTMLTextAreaElement>(null);
+
+	const remarkable = new Remarkable({
+		highlight: function (str, lang) {
+			if (lang && hljs.getLanguage(lang)) {
+				try {
+					return hljs.highlight(lang, str).value;
+				} catch (err) {}
+			}
+
+			try {
+				return hljs.highlightAuto(str).value;
+			} catch (err) {}
+
+			return "";
+		},
+		html: true,
+		typographer: true,
+		breaks: true,
+		xhtmlOut: true,
+	});
+
+	const parse = (markdown: string) => {
+		div.current!.innerHTML = remarkable.render(markdown);
+	};
+
+	const resizeTextArea = () => {
+		textarea.current!.style.height = "auto";
+		textarea.current!.style.height = `${textarea.current!.scrollHeight + 20}px`;
+	};
+
+	useEffect(() => {
+		parse(markdownInput);
+		resizeTextArea();
+	}, [markdownInput]);
 
 	return (
 		<Container className="mb-16">
@@ -54,14 +91,17 @@ export default function App(): JSX.Element {
 			<div className="flex gap-x-4 flex-col gap-y-4 lg:flex-row">
 				<Section>
 					<textarea
-						className="w-full lg:min-h-screen form-textarea rounded border-gray-300 focus:border-gray-400 focus:ring-gray-400"
+						className="w-full whitespace-nowrap lg:min-h-screen form-textarea rounded border-gray-300 focus:border-gray-400 focus:ring-gray-400"
 						ref={textarea}
-						rows={10}
 						autoFocus={true}
+						onChange={(e) => setMarkdownInput(e.target.value)}
 					></textarea>
 				</Section>
 				<Section>
-					<h1>Hello World!</h1>
+					<div
+						className="prose prose-sky break-words prose-a:inline-block dark:prose-invert prose-headings:border-b-2 prose-headings:pb-4"
+						ref={div}
+					></div>
 				</Section>
 			</div>
 		</Container>
