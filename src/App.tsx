20f0e4f234ca387a0ebc4047f991d0a9a4db5f0c
theozebua <ronizebua.rz@gmail.com>
2022-12-29 07:34:14 +0700

    Create tooltip functionality

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ export default function App(): JSX.Element {
 				break;
 
 			case "code":
-				convertedValue = `<code>${value}</code>`;
+				convertedValue = `\`${value}\``;
 				break;
 
 			default:
@@ -123,34 +123,60 @@ export default function App(): JSX.Element {
 			</div>
 			<div className="mb-4 flex flex-col gap-4 md:flex-row">
 				<button
-					className="relative max-w-fit rounded bg-white p-4 shadow hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+					className="relative max-w-fit rounded bg-white p-4 shadow transition hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+					data-toggle="tooltip"
+					data-title="Tools"
 					onClick={() => setShowTools(!showTools)}
 				>
 					<WrenchScrewdriverIcon className="h-6 w-6" />
 				</button>
 				<div className={`tools ${showTools ? "show" : "hide"}`}>
-					<button className="btn-tools" onClick={(e) => useTool(e, "bold")}>
+					<button
+						className="btn-tools"
+						onClick={(e) => useTool(e, "bold")}
+						data-toggle="tooltip"
+						data-title="Bold"
+					>
 						<FontAwesomeIcon icon={faBold} />
 					</button>
-					<button className="btn-tools" onClick={(e) => useTool(e, "italic")}>
+					<button
+						className="btn-tools"
+						onClick={(e) => useTool(e, "italic")}
+						data-toggle="tooltip"
+						data-title="Italic"
+					>
 						<FontAwesomeIcon icon={faItalic} />
 					</button>
 					<button
 						className="btn-tools"
 						onClick={(e) => useTool(e, "underline")}
+						data-toggle="tooltip"
+						data-title="Underline"
 					>
 						<FontAwesomeIcon icon={faUnderline} />
 					</button>
 					<button
 						className="btn-tools"
 						onClick={(e) => useTool(e, "strikethrough")}
+						data-toggle="tooltip"
+						data-title="Strikethrough"
 					>
 						<FontAwesomeIcon icon={faStrikethrough} />
 					</button>
-					<button className="btn-tools" onClick={(e) => useTool(e, "code")}>
+					<button
+						className="btn-tools"
+						onClick={(e) => useTool(e, "code")}
+						data-toggle="tooltip"
+						data-title="Code"
+					>
 						<FontAwesomeIcon icon={faCode} />
 					</button>
-					<button className="btn-tools" onClick={(e) => useTool(e, "quote")}>
+					<button
+						className="btn-tools"
+						onClick={(e) => useTool(e, "quote")}
+						data-toggle="tooltip"
+						data-title="Quote"
+					>
 						<FontAwesomeIcon icon={faQuoteRight} />
 					</button>
 				</div>
@@ -158,7 +184,7 @@ export default function App(): JSX.Element {
 			<div className="flex flex-col gap-4 lg:flex-row">
 				<Section>
 					<textarea
-						className="form-textarea w-full whitespace-nowrap rounded border-gray-300 focus:border-gray-400 focus:ring-gray-400 dark:border-gray-700 dark:bg-gray-800 dark:focus:border-gray-600 dark:focus:ring-gray-600 lg:min-h-screen"
+						className="form-textarea w-full resize-none whitespace-nowrap rounded border-gray-300 focus:border-gray-400 focus:ring-gray-400 dark:border-gray-700 dark:bg-gray-800 dark:focus:border-gray-600 dark:focus:ring-gray-600 lg:min-h-screen"
 						ref={textarea}
 						autoFocus={true}
 						onChange={(e) => setMarkdownInput(e.target.value)}
@@ -166,7 +192,7 @@ export default function App(): JSX.Element {
 				</Section>
 				<Section>
 					<div
-						className="prose prose-sky min-w-full break-words prose-headings:border-b-2 prose-headings:pb-4 prose-a:inline-block dark:prose-invert dark:prose-headings:border-b-gray-300 dark:prose-headings:text-gray-300"
+						className="prose prose-sky min-w-full break-words prose-headings:border-b-2 prose-headings:pb-4 prose-a:inline-block prose-a:no-underline prose-img:inline-block dark:prose-invert dark:prose-headings:border-b-gray-300 dark:prose-headings:text-gray-300"
 						ref={div}
 					></div>
 				</Section>
