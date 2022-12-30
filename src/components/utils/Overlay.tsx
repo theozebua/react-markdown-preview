c73b2d5d513c509aaf48b502714349bb5b4ab5d1
theozebua <ronizebua.rz@gmail.com>
2022-12-30 21:27:14 +0700

    Remove unnecessary css class

--- src/components/utils/Overlay.tsx
+++ src/components/utils/Overlay.tsx
@@ -6,7 +6,7 @@ export default function Overlay({ showModal }: Props): JSX.Element {
 	return (
 		<div
 			className={`fixed top-0 bottom-0 left-0 right-0 z-40 bg-gray-900/50 backdrop-blur-sm ${
-				showModal ? "block bg-gray-900/50 backdrop-blur-sm" : "hidden"
+				showModal ? "block" : "hidden"
 			}`}
 		/>
 	);
